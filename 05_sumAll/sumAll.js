7d002d724758444ecf509983e278567711c39ea2
Kevin Chen <kevin5112@yahoo.com>
2023-09-26 16:16:33 -0700

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if(!Number.isInteger(min) || !Number.isInteger(max) || min < 0 || max < 0) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    for(let i = Math.min(min, max); i <= Math.max(min, max); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
