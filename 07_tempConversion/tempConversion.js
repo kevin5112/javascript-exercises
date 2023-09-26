8363dd12bc703ebe40c3113faa3132f15d11c252
Kevin Chen <kevin5112@yahoo.com>
2023-09-26 16:44:57 -0700

    complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  return Math.round(((number-32) * (5/9)) * 10)/10;
+  // if(ans % 1 != 0) {
+  //   return ans.toFixed(1);
+  // } else {
+  //   return ans.toFixed(0);
+  // }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  return Math.round((number * (9/5) + 32)*10)/10;
+  // if(ans % 1 != 0) { 
+  //   return ans.toFixed(1);
+  // } else {
+  //   return ans.toFixed(0);
+  // }
 };
 
 // Do not edit below this line
