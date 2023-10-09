7a8fa2981bf74c3129be33f2c8cfb438fdcd4133
Kevin Chen <kevin5112@yahoo.com>
2023-10-09 15:49:26 -0700

    complete exercise 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[^\w\']|_\s/g, "").toLowerCase();
+    console.log(str);
+    let start = 0;
+    let end = str.length - 1;
+    while(start < end) {
+        if(str[start] != str[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
+    if(num < 0) {
+        return "OOPS";
+    }
 
+    if(num === 1 || num === 2) {
+        return 1;
+    }
+
+    let x = 1;
+    let y = 1;
+    let z = 0;
+    for(let i = 2; i < num; ++i) {
+        z = x + y;
+        if(x < y) {
+            x = z;
+        } else {
+            y = z;
+        }
+    }
+    return z;
 };
 
 // Do not edit below this line
