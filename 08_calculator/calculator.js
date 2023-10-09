a42275ed8e5e14ef1f7fc0fbd3fcaf5ea74625c5
Kevin Chen <kevin5112@yahoo.com>
2023-10-09 15:14:41 -0700

    do exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let total = 0;
+  nums.forEach(x => total += x);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let total = 1;
+  nums.forEach(x => total *= x);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+  let total = 1;
+  while(exponent > 0) {
+    total *= num;
+    --exponent;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let total = 1;
+	while(x > 0) {
+    total *= x;
+    --x;
+  }
+  return total;
 };
 
 // Do not edit below this line
