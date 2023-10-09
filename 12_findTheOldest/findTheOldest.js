f082677e1f743fd3481ab01c9eb531a953620d9c
Kevin Chen <kevin5112@yahoo.com>
2023-10-09 16:25:31 -0700

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((person, current) => {
+        let oldestAge;
+        if('yearOfDeath' in person) oldestAge = person.yearOfDeath - person.yearOfBirth;
+        else oldestAge = new Date().getFullYear() - person.yearOfBirth;
+        let newAge;
+        if('yearOfDeath' in current) newAge = current.yearOfDeath - current.yearOfBirth;
+        else newAge = new Date().getFullYear() - current.yearOfBirth;
 
+        console.log(person, current);
+        console.log(oldestAge, newAge);
+        return oldestAge > newAge ? person : current
+
+    });
 };
 
 // Do not edit below this line
